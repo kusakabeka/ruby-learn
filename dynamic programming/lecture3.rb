79613f8160eeffca347297a21f541beed64c8248
kusakabeka <n.kuzmingg@gmail.com>
2023-02-26 14:37:31 +0300

    dynamic programming

--- dynamic programming/lecture3.rb
+++ dynamic programming/lecture3.rb
@@ -1,3 +1,14 @@
+=begin
+    problems:
+    find the sum of the fires N numbers
+
+    Objective function:
+    f(i) id the sum of the first i elements
+
+    recurrence relation:
+    f(n) = f(n-1) + n
+=end
+
 def nSum(n)
   dp = []
   dp[0] = 0
